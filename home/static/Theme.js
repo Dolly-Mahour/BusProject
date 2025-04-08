5a2a45269f0c02162d65104330931337dca16ce1
Dolly-Mahour <dollymahour282@gmail.com>
2025-04-08 17:08:26 +0530

    theme change without (colors are not compatible)

--- home/static/Theme.js
+++ home/static/Theme.js
@@ -10,13 +10,32 @@ function setTheme()
             console.log('checked')
             document.documentElement.setAttribute('data-theme', 'dark');
             document.getElementById('busifyLogo').src = "../home/static/peach-logo.png";
+            document.getElementById('download-img').src = "../home/static/download-peach.png";
+            document.getElementById('globe-image').src = "../home/static/globe-peach.png";
+            document.getElementById('toggle-btn-img').src = "../home/static/toggle-peach.png";
+
+            document.getElementById('email-img').src = "../home/static/email-dark-theme.png";
+            document.getElementById('call-img').src = "../home/static/call-dark-theme.png";
+            document.getElementById('location-img').src = "../home/static/location-dark-theme.png";
+
             window.localStorage.setItem('data-theme', 'dark');
         } 
         else
         {
           console.log("unchecked")
-          document.documentElement.setAttribute('data-theme', 'light');
-          window.localStorage.setItem('data-theme', 'light');
+
+            document.getElementById('busifyLogo').src = "../home/static/logo.png";
+            document.getElementById('download-img').src = "../home/static/download.png";
+            document.getElementById('globe-image').src = "../home/static/planet-earth.png";
+            document.getElementById('toggle-btn-img').src = "../home/static/hamburger.png";
+
+            document.getElementById('email-img').src = "../home/static/email.png";
+            document.getElementById('call-img').src = "../home/static/phone.png";
+            document.getElementById('location-img').src = "../home/static/location.png";
+
+            document.documentElement.setAttribute('data-theme', 'light');
+            window.localStorage.setItem('data-theme', 'light');
+          
         }
     }
 
