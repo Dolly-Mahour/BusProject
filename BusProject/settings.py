1ef1d87dd1604c2cfc00d8d89d56fe8af8327245
Bajeshkumar <77668520+Bajeshkumar@users.noreply.github.com>
2025-07-29 22:23:25 +0530

    Deploying BusProject to Render

--- BusProject/settings.py
+++ BusProject/settings.py
@@ -186,7 +186,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 LOCALE_PATHS = [
-    os.path.join(BASE_DIR, 'locale')
+    os.path.join(BASE_DIR, 'locale') 
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
